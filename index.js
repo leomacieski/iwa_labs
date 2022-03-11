db9d4cdcce14faae57c09888aa94be1251f60063
leomacieski <leojkmacieski@Hotmail.com>
2022-03-11 09:44:43 +0000

    week3

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ axios = require("axios");
 
 http.createServer((req,res)=>{
     res.write (users.join(", "));
-    res.write (email.join(", "));
+    res.write("\n\n"+email.join(", "));
     res.end()
 }).listen(8000);
 
@@ -14,14 +14,6 @@ let email = [];
     try{
         const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
         users = data.map(user=>user.name);
-    }catch(error){
-        console.log(error)
-    }
-})();
-
-(async function getEmail(){
-    try{
-        const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
         email = data.map(user=>user.email);
     }catch(error){
         console.log(error)
