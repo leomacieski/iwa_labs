62feb323ea41b215cef336490e7745f460c7f180
leomacieski <leojkmacieski@Hotmail.com>
2022-03-11 10:42:07 +0000

    routes.js

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
-const http = require("http"),
-axios = require("axios");
+const   http = require("http"),
+        logger = require("morgan"),
+        express = require("express"),
+        bodyParser = require("body-parser");
 
-http.createServer((req,res)=>{
-    res.write (users.join(", "));
-    res.write("\n\n"+email.join(", "));
-    res.end()
-}).listen(8000);
+let app = express();
+let port = 8000;
 
-let users = [];
-let email = [];
+app.use(require('./routes'));
+app.use(bodyParser.json());
+app.use(logger("tiny"));
 
-(async function getNames(){
-    try{
-        const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
-        users = data.map(user=>user.name);
-        email = data.map(user=>user.email);
-    }catch(error){
-        console.log(error)
-    }
-})();
+
+
+app.listen(port, function(err){
+    console.log("Listening on port: " + port)
+});
