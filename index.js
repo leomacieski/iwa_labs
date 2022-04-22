00c8e301a9a8fd4d30b53a6835cf8ff373a2018b
leomacieski <leojkmacieski@Hotmail.com>
2022-04-22 10:11:49 +0000

    mongodb atlas

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 const   http = require("http"),
         logger = require("morgan"),
         express = require("express"),
-        bodyParser = require("body-parser");
-        mongoose = require("mongoose");
+        bodyParser = require("body-parser"),
+        mongoose = require("mongoose"),
+        dotenv = require("dotenv");
 
 let app = express();
-let port = 8000;
+let port = process.env.port || 8000;
+
+dotenv.config();
 
 app.use(express.json());
 app.use(logger("tiny"));
 app.use(require('./routes'));
 
 
-const dbURI = "mongodb://localhost/test";
-
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then((result) => console.log('connected to db'))
         .catch((err) => console.log(err));
 
